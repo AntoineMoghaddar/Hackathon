d39f4a54bab48f0064315de9b0dd4467de08ec53
Antoine Moghaddar <a.a.moghaddar@student.utwente.nl>
2020-10-27 12:17:26 +0100

    init model

--- src/javamain/HackathonModel/Main.java
+++ src/javamain/HackathonModel/Main.java
@@ -1,9 +1,40 @@
 package javamain.HackathonModel;
 
-public class Main {
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-    public void run(){
-        System.out.println("Print init");
+public class Main {
+    private Integer[][] board;
+
+    public void run() {
+        board = new Integer[64][64];
+        makeModel();
+        modelprint();
+    }
+
+    public void makeModel() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    /* Value descriptions
+     *   -1 = obstacle
+     *   0 = not explored
+     *   1 = Current position
+     *   2 = optimal path
+     *   3 = next node
+     *   9 = End note
+     */
+    public void modelprint() {
+        for (int j = 0; j < board.length; j++) {
+            for (int i = 0; i < board.length; i++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
